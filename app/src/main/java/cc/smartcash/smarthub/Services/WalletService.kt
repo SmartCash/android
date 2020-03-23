b86b1fb4b8dd228943dea85b6297416e594739e2
enriquesouza <enriquesouza>
2020-03-23 10:58:01 -0300

    Change to .NET Core API

--- app/src/main/java/cc/smartcash/smarthub/Services/WalletService.kt
+++ app/src/main/java/cc/smartcash/smarthub/Services/WalletService.kt
@@ -1,10 +1,10 @@
 package cc.smartcash.smarthub.Services
 
-import cc.smartcash.smarthub.Models.*
-import cc.smartcash.smarthub.ViewModels.WalletViewModel
+import cc.smartcash.smarthub.Models.SendPayment
+import cc.smartcash.smarthub.Models.WalletPaymentFeeRequest
+import cc.smartcash.smarthub.Models.WebWalletRootResponse
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -13,7 +13,7 @@ interface WalletService {
     @POST("wallet/sendpayment")
     fun sentPayment(@Header("Authorization") auth: String, @Body sendPayment: SendPayment): Call<WebWalletRootResponse<String>>
 
-    @POST("wallet/paymentfee")
+    @POST("wallet/GetPaymentFee")
     fun getFee(@Header("Authorization") auth: String, @Body feeRequest: WalletPaymentFeeRequest): Call<WebWalletRootResponse<Double>>
 
 }
--- app/src/main/java/cc/smartcash/smarthub/Utils/URLS.kt
+++ app/src/main/java/cc/smartcash/smarthub/Utils/URLS.kt
@@ -2,24 +2,13 @@ package cc.smartcash.smarthub.Utils
 
 object URLS {
 
-    const val URL_API_EXPLORER = "https://api-explorer.smartcash.cc/api/smartexplorer/"
-
     const val URL_API_SAPI = "https://sapi2.smartcash.org/v1/"
 
-    ////const val URL_API_WEBWALLET = "https://smartcashapiprod.azurewebsites.net/api/"
     const val URL_API_WEBWALLET = "https://api.smartcash.org/api/"
-
 
     const val URL_API_PRICE = "https://api.smartcash.cc/v1/"
 
-    const val URL_API_PRICE_HITBTC = "https://api.smartcash.cc/v1/exchange/currencies?exchange=hitbtc"
-
-    //const val URL_API_PRICE_COINGECKO = "https://api.smartcash.cc/v1/exchange/currencies?exchange=coingecko"
-
     const val URL_API_PRICE_COINGECKO = "https://api.coingecko.com/api/v3/"
-
-
-    const val URL_API_ = ""
 
     const val URL_INSIGHT_EXPLORER_API = "https://insight.smartcash.cc/api/tx/"
 
@@ -27,15 +16,8 @@ object URLS {
 
     const val URL_INSIGHT_EXPLORER = "https://insight.smartcash.cc/tx/"
 
-    const val URL_CURRENT_PRICE_API = "https://wallet.smartcash.cc/api/"
-
-    const val URL_QRCODE_AMOUNT = "https://qr.smartcash.cc/?address=%s&amount=%f"
-
-    const val URL_QRCODE = "https://qr.smartcash.cc/?address=%s"
-
     const val URL_CHANGE_PASSWORD = "https://wallet.smartcash.cc/change-password"
 
     const val URL_API_SMARTTEXT = "https://smartext.me/"
-
 
 }
